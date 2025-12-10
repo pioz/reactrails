1ffaf75ce1088850458c2c15c702b91683666652
pioz <epilotto@gmx.com>
2025-12-10 17:54:11 +0100

    Improve warning message

--- app/javascript/common.js
+++ app/javascript/common.js
@@ -1,15 +1,14 @@
-const warningMessage = (
-  componentName
-) => `React component "${componentName}" not found in globalThis.ReactRailsComponents registry.
+const warningMessage = (componentName) => `React component "${componentName}" is not registered.
 
 Add your component in the React Rails registry:
 
+import registerComponents from 'reactrails/registerComponents'
 import ${componentName} from './${componentName}'
-globalThis.ReactRailsComponents = {
+registerComponents({
   ${componentName},
   Component1,
   Component2
-}`
+})`
 
 const loadProps = (componentName, jsonStringProps) => {
   jsonStringProps ||= '{}'
