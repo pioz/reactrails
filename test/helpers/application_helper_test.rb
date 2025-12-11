b539c16704f13a3a971d8ec0eb0d0c27fb9999c9
pioz <epilotto@gmx.com>
2025-12-11 17:47:24 +0100

    Add test

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class ApplicationHelperTest < ActiveSupport::TestCase
+  test "render_component" do
+    skip "TODO"
+  end
+
+  test "render_component with span div" do
+    skip "TODO"
+  end
+
+  test "render_component with html options" do
+    skip "TODO"
+  end
+
+  test "render_component with prerender" do
+    skip "TODO"
+  end
+
+  test "render_component raises if tag is not valid" do
+    skip "TODO"
+  end
+end
