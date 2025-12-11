e1123e459c5d9a2616055d9a7f152c115979658d
pioz <epilotto@gmx.com>
2025-12-11 16:46:09 +0100

    Raise SSR bundle not found

--- lib/reactrails/react_renderer.rb
+++ lib/reactrails/react_renderer.rb
@@ -25,6 +25,10 @@ class Reactrails::ReactRenderer
       ssr_preload_code = Reactrails.config.ssr_preload_code
       server_bundle_path = Reactrails::Engine.root.join("app/assets/builds/reactrails.js")
       ssr_init_reactrails_bundle_path = Reactrails.config.ssr_init_reactrails_bundle_path
+      [server_bundle_path, ssr_init_reactrails_bundle_path].each do |path|
+        raise "SSR bundle not found: #{path}" unless File.exist?(path)
+      end
+
       [ssr_preload_code, File.read(server_bundle_path), File.read(ssr_init_reactrails_bundle_path)].compact.join("\n")
     end
   end
